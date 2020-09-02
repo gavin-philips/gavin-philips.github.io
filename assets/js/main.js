df6efce1bd154048a229ca428ff843e89bd78ed9
gavin-philips <philips.gavin@gmail.com>
2020-09-02 16:06:51 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -298,7 +298,7 @@
 					var $this = $(this);
 
 					// Close.
-						$('<div class="close">Close</div>', {type: 'button'})	/* Gavin modified */
+						$('<div class="close">Close</div>')	/* Gavin modified */
 							.appendTo($this)
 							.on('click', function() {
 								location.hash = '';
